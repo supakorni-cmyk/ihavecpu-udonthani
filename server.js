430bdd9dd4fcf9886a893f22ffadaef32176f7fe
supakorni-cmyk <supakorn.i@ihavecpu.com>
2026-03-30 11:53:22 +0700

    2nd test

--- server.js
+++ server.js
@@ -44,9 +44,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // --- In-Memory Mock Database (Organized by Zones) ---
 const adSpots = {
-"exteriorLogo": {
+  "exteriorLogo": {
     "name": "Exterior Logo",
-    "layoutImages": ["images/exterior _logo1.jpg", "images/logo2.jpg"],
+    "layoutImages": ["images/2.jpg"],
     "spots": {
       "EX1": { "name": "EX1", "size": "1920x1080px", "price": 30000, "status": "Available", "bookedBy": "" },
       "EX2": { "name": "EX2", "size": "1920x1080px", "price": 30000, "status": "Available", "bookedBy": "" },
@@ -55,16 +55,7 @@ const adSpots = {
       "EX5": { "name": "EX5", "size": "1920x1080px", "price": 30000, "status": "Available", "bookedBy": "" },
       "EX6": { "name": "EX6", "size": "1920x1080px", "price": 30000, "status": "Available", "bookedBy": "" },
       "EX7": { "name": "EX7", "size": "1920x1080px", "price": 30000, "status": "Available", "bookedBy": "" },
-      "EX8": { "name": "EX8", "size": "1920x1080px", "price": 30000, "status": "Available", "bookedBy": "" },
-      "EX9": { "name": "EX9", "size": "1920x1080px", "price": 30000, "status": "Available", "bookedBy": "" },
-      "EX10": { "name": "EX10", "size": "1920x1080px", "price": 30000, "status": "Available", "bookedBy": "" },
-      "EX11": { "name": "EX11", "size": "1920x1080px", "price": 30000, "status": "Available", "bookedBy": "" },
-      "EX12": { "name": "EX12", "size": "1920x1080px", "price": 30000, "status": "Available", "bookedBy": "" },
-      "EX13": { "name": "EX13", "size": "1920x1080px", "price": 30000, "status": "Available", "bookedBy": "" },
-      "EX14": { "name": "EX14", "size": "1920x1080px", "price": 30000, "status": "Available", "bookedBy": "" },
-      "EX15": { "name": "EX15", "size": "1920x1080px", "price": 30000, "status": "Available", "bookedBy": "" },
-      "EX16": { "name": "EX16", "size": "1920x1080px", "price": 30000, "status": "Available", "bookedBy": "" },
-      "EX17": { "name": "EX17", "size": "1920x1080px", "price": 30000, "status": "Available", "bookedBy": "" }
+      "EX8": { "name": "EX8", "size": "1920x1080px", "price": 30000, "status": "Available", "bookedBy": "" }
     }
   },
   "exteriorSticker": {
@@ -80,38 +71,45 @@ const adSpots = {
   },
   "interiorSticker": {
     "name": "Interior Sticker",
-    "layoutImages": ["images/ist1-2.jpg", "images/ist3.jpg", "images/ist4-5.jpg", "images/ist6.jpg"],
+    "layoutImages": ["images/ist-1.jpg", "images/ist-2-5.jpg", "images/ist-6-8.jpg", "images/ist-9.jpg"],
     "spots": {
       "IST-1": { "name": "IST-1", "size": "3m x 1.5m", "price": 40000, "status": "Available", "bookedBy": "" },
       "IST-2": { "name": "IST-2", "size": "3m x 1.5m", "price": 40000, "status": "Available", "bookedBy": "" },
       "IST-3": { "name": "IST-3", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
       "IST-4": { "name": "IST-4", "size": "3m x 1.5m", "price": 35000, "status": "Available", "bookedBy": "" },
       "IST-5": { "name": "IST-5", "size": "3m x 1.5m", "price": 35000, "status": "Available", "bookedBy": "" },
-      "IST-6": { "name": "IST-6", "size": "3m x 1.5m", "price": 35000, "status": "Available", "bookedBy": "" }
+      "IST-6": { "name": "IST-6", "size": "3m x 1.5m", "price": 35000, "status": "Available", "bookedBy": "" },
+      "IST-7": { "name": "IST-7", "size": "3m x 1.5m", "price": 35000, "status": "Available", "bookedBy": "" },
+      "IST-8": { "name": "IST-8", "size": "3m x 1.5m", "price": 35000, "status": "Available", "bookedBy": "" },
+      "IST-9": { "name": "IST-9", "size": "3m x 1.5m", "price": 35000, "status": "Available", "bookedBy": "" }
     }
   },
   "interiorLogo": {
     "name": "Interior Logo",
-    "layoutImages": ["images/1.jpg", "images/2.jpg", "images/3.jpg", "images/4.jpg","images/5.jpg"],
+    "layoutImages": ["images/6.jpg", "images/11.jpg", "images/12.jpg", "images/9.jpg"],
     "spots": {
-      "A1": { "name": "A1 - Monitor", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
-      "A2": { "name": "A2 - Monitor", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
-      "A3": { "name": "A3 - Monitor", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
-      "A4": { "name": "A4 - Monitor", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
+      "A1": { "name": "A1 - CPU", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
+      "A2": { "name": "A2 - CPU", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
+      "A3": { "name": "A3 - Case & Cooling", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
+      "A4": { "name": "A4 - Case & Cooling", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
       "A5": { "name": "A5 - Case & Cooling", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
       "A6": { "name": "A6 - Case & Cooling", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
       "A7": { "name": "A7 - Case & Cooling", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
       "A8": { "name": "A8 - Case & Cooling", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
-      "A9": { "name": "A9 - CPU", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
+      "A9": { "name": "A9 - DIY", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
       "A10": { "name": "A10 - DIY", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
       "A11": { "name": "A11 - DIY", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
       "A12": { "name": "A12 - DIY", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
       "A13": { "name": "A13 - DIY", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
-      "A14": { "name": "A14 - DIY", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
+      "A14": { "name": "A14 - Gaming Gear", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
       "A15": { "name": "A15 - Gaming Gear", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
       "A16": { "name": "A16 - Gaming Gear", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
       "A17": { "name": "A17 - Gaming Gear", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
-      "A18": { "name": "A18 - Gaming Gear", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" }
+      "A18": { "name": "A18 - Gaming Gear", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
+      "A19": { "name": "A19 - Monitor", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
+      "A20": { "name": "A20 - Monitor", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
+      "A21": { "name": "A21 - Monitor", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" },
+      "A22": { "name": "A22 - Monitor", "size": "3m x 1.5m", "price": 30000, "status": "Available", "bookedBy": "" }
     }
   }
 };
